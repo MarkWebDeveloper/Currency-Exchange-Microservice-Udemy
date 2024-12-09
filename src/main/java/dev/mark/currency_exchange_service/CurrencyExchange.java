8791095ec9f346c961905696a292ff6ef0e40d1c
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-12-09 12:26:40 +0100

    connected the h2 database

--- src/main/java/dev/mark/currency_exchange_service/CurrencyExchange.java
+++ src/main/java/dev/mark/currency_exchange_service/CurrencyExchange.java
@@ -2,21 +2,29 @@ package dev.mark.currency_exchange_service;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class CurrencyExchange {
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
-    private BigDecimal convertionMultiple;
+    private BigDecimal conversionMultiple;
     private String environment;
     
     public CurrencyExchange() {
     }
 
-    public CurrencyExchange(Long id, String from, String to, BigDecimal convertionMultiple) {
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
         this.id = id;
         this.from = from;
         this.to = to;
-        this.convertionMultiple = convertionMultiple;
+        this.conversionMultiple = conversionMultiple;
     }
 
     public Long getId() {
@@ -43,12 +51,12 @@ public class CurrencyExchange {
         this.to = to;
     }
 
-    public BigDecimal getConvertionMultiple() {
-        return convertionMultiple;
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
     }
 
-    public void setConvertionMultiple(BigDecimal convertionMultiple) {
-        this.convertionMultiple = convertionMultiple;
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
     }
 
     public String getEnvironment() {
