9840c1c30e14a67e4233b029e9ac932ee03ab3ef
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-12-09 11:45:25 +0100

    currency exchange controller and model are ready

---
+++ src/test/java/dev/mark/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mark.currency_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
