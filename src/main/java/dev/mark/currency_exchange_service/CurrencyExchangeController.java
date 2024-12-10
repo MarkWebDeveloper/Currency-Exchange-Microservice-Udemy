123b126d58e7b7a7fdf68853fe13a3d1b9c122ab
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-12-10 13:28:33 +0100

    connected to the database

--- src/main/java/dev/mark/currency_exchange_service/CurrencyExchangeController.java
+++ src/main/java/dev/mark/currency_exchange_service/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package dev.mark.currency_exchange_service;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeController {
 
     @Autowired
+    private CurrencyExchangeRepository repository;
+
+    @Autowired
     private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
         String port = environment.getProperty("local.server.port");
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unableto find data for " + from + " to " + to);
+        }
         currencyExchange.setEnvironment(port);
         return currencyExchange;
     }
